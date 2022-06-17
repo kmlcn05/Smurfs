5dd7a038724ab267a8d5e785feae358873c87067
Mehmet Aygun <mehmetaygun3638@gmail.com>
2022-06-17 14:15:26 +0300

    IndexPage

---
+++ Smurfs.WebUI/Controllers/IndexPageController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Smurfs.WebUI.Controllers
+{
+    public class IndexPageController : Controller
+    {
+        public IActionResult AnaSayfa()
+        {
+            return View();
+        }
+    }
+}
--- Smurfs.WebUI/Program.cs
+++ Smurfs.WebUI/Program.cs
@@ -25,6 +25,6 @@ app.UseSession();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=Login}/{action=Login}/{id?}");
 
 app.Run();
