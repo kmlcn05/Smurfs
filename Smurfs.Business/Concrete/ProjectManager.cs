deded24142d5d8a13766838336da720aeaa1df36
Kemalcan <65194440+kmlcn05@users.noreply.github.com>
2022-06-21 23:50:20 +0300

    Parametreler
    parametreler yapıldı

--- Smurfs.Business/Concrete/ProjectManager.cs
+++ Smurfs.Business/Concrete/ProjectManager.cs
@@ -1,6 +1,7 @@
 ﻿using Smurfs.Business.Abstract;
 using Smurfs.Core.Abstract;
 using Smurfs.Entities.Conrete;
+using Smurfs.Entity.Concrete;
 
 namespace Smurfs.Business.Concrete
 {
@@ -41,27 +42,27 @@ namespace Smurfs.Business.Concrete
             _unitofwork.Project.Delete(entity);
             _unitofwork.Save();
         }
-       /* public Project Calculate(int projectId)
+       public ProjectParameters Calculate(int projectId)
         {
-            var projects = _unitofwork.Project.GetById(projectId);
-            if (projects != null)
+            var project = _unitofwork.Project.GetById(projectId);
+            if (project != null)
             {
-                Project project = new Project();
-                project.ProjeVerimYuzdesi = (project.ProjeGerceklesen / project.ProjeKapasite) * 100;
-                project.ProjeVerimDegeri = (project.ProjeGerceklesen - project.ProjeKapasite);
+                ProjectParameters projectparameter = new ProjectParameters();
+                projectparameter.ProjeVerimYuzdesi = (projectparameter.ProjeGerceklesen / projectparameter.ProjeKapasite) * 100;
+                projectparameter.ProjeVerimDegeri = (projectparameter.ProjeGerceklesen - projectparameter.ProjeKapasite);
 
-                if (project.ProjeVerimDegeri < 0)
+                if (projectparameter.ProjeVerimDegeri < 0)
                 {
-                    project.ProjeVerimDegeri = 0;
+                    projectparameter.ProjeVerimDegeri = 0;
                 }
                 else
                 {
-                    project.ProjeVerimDegeri = project.ProjeVerimDegeri;
+                    projectparameter.ProjeVerimDegeri = projectparameter.ProjeVerimDegeri;
                 }
-                project.ProjeVerimSonucu = (project.ProjeVerimDegeri * project.ProjeCarpani);
+                projectparameter.ProjeVerimSonucu = (projectparameter.ProjeVerimDegeri * projectparameter.ProjeCarpani);
 
-                Project projecthesap = new Project();
-                projecthesap = project;
+                ProjectParameters projecthesap = new ProjectParameters();
+                projecthesap = projectparameter;
 
                 return projecthesap;
             }
@@ -70,7 +71,7 @@ namespace Smurfs.Business.Concrete
                 throw new Exception("Project Not found!");
             }
 
-        }*/
+        }
 
     }
 }
