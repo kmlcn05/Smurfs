e98805a5eef5025338e4b5a81186a48223e532d8
Kemalcan <65194440+kmlcn05@users.noreply.github.com>
2022-06-22 13:09:45 +0300

    ParametrelerV2
    tekrar yapıldı

--- Smurfs.API/Controllers/ProjectController.cs
+++ Smurfs.API/Controllers/ProjectController.cs
@@ -7,68 +7,69 @@ namespace Smurfs.API.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class ProjectController:ControllerBase
+    public class ProjectController : ControllerBase
     {
-            
-            private IProjectService _projectService;
 
-            public ProjectController(IProjectService projectService)
+        private IProjectService _projectService;
+
+        public ProjectController(IProjectService projectService)
+        {
+            _projectService = projectService;
+        }
+
+        // GET: api/<ProjectController>
+        [HttpGet("GetAll")]
+        public async Task<IActionResult> GetAll()
+        {
+            var Project = await _projectService.GetAll();
+            return Ok(Project);
+        }
+
+        // GET api/<ProjectController>/5
+        [HttpGet("GetByIdp")]
+        public async Task<IActionResult> GetById(int Id)
+        {
+            var p = await _projectService.GetById(Id);
+            if (p == null)
             {
-                _projectService = projectService;
+                return NotFound();
             }
 
-            // GET: api/<ProjectController>
-            [HttpGet]
-            public async Task<IActionResult> GetAll()
-            {
-                var Project = await _projectService.GetAll();
-                return Ok(Project);
-            }
+            return Ok(p);
+        }
 
-            // GET api/<ProjectController>/5
-            [HttpGet("{id}")]
-            public async Task<IActionResult> GetById(int Id)
-            {
-                var p = await _projectService.GetById(Id);
-                if (p == null)
-                {
-                    return NotFound();
-                }
+        // POST api/<ProjectController>/5
+        [HttpPost("Save")]
+        public IActionResult SaveProject([FromBody] Project project)
+        {
+            _projectService.Create(project);
+            return Ok(project);
+        }
 
-                return Ok(p);
-            }
+        // PUT api/<ProjectController>/5
+        [HttpPut("Update")]
+        public IActionResult Update([FromBody] Project project)
+        {
+            _projectService.Update(project);
+            return Ok(project);
+        }
 
-            // POST api/<ProjectController>/5
-            [HttpPost]
-                public IActionResult SaveProject([FromBody] Project project)
-                {
-                    _projectService.Create(project);
-                    return Ok(project);
-                }
-
-            // PUT api/<ProjectController>/5
-            [HttpPut]
-                public IActionResult Update([FromBody] Project project)
-                {
-                    _projectService.Update(project);
-                    return Ok(project);
-                }
-
-            // DELETE api/<ProjectController>/5
-            [HttpDelete]
-                public IActionResult Delete([FromBody] Project project)
-                {
-                    _projectService.Delete(project);
-                    return Ok("Silindi");
-                }
+        // DELETE api/<ProjectController>/5
+        [HttpDelete("Delete")]
+        public IActionResult Delete([FromBody] Project project)
+        {
+            _projectService.Delete(project);
+            return Ok("Silindi");
+        }
 
 
-                //[HttpPost]
-                //public IActionResult Calculate([FromBody] int projectId)
-                //{
-                //    _projectService.Calculate(projectId);
-                //    return Ok("Hesaplamalar doğru şekilde yapıldı");
-                //}
-                
+        // CALCULATE api/<ProjectController>/5
+        [HttpPost("Calculate")]
+        public IActionResult Calculate([FromBody] int projectId)
+        {
+            _projectService.Calculate(projectId);
+            return Ok("Hesaplamalar doğru şekilde yapıldı");
+        }
+
     }
 }
