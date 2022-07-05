a0fb6b58676a23a4c90edc9177812a93b12def7f
Zeynep Ece Kul <66221991+zeynepkul@users.noreply.github.com>
2022-07-05 16:10:58 +0300

    Şifre değiştirme ekranı oluşturuldu UI

---
+++ Smurfs.WebUI/Controllers/SifreDegistirController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Smurfs.WebUI.Controllers
+{
+    public class SifreDegistirController : Controller
+    {
+        public IActionResult SifreDegistir()
+        {
+            return View();
+        }
+    }
+}
